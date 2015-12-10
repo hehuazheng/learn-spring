4c12b855fb0cf1d3858087f6b673328cc63452a6
hejiafu <hejiafu@secoo.com>
2015-12-10 20:35:01 +0800

    add

---
+++ src/main/java/study/component/TestInjectionComponent.java
@@ -0,0 +1,13 @@
+package study.component;
+
+public class TestInjectionComponent {
+	private String testProperty;
+
+	public String getTestProperty() {
+		return testProperty;
+	}
+
+	public void setTestProperty(String testProperty) {
+		this.testProperty = testProperty;
+	}
+}
--- src/main/java/study/controllers/TestController.java
+++ src/main/java/study/controllers/TestController.java
@@ -5,13 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import study.component.TestInjectionComponent;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
-
+	@Autowired
+	private TestInjectionComponent testInjectionComponent;
+	
 	@ResponseBody
 	@RequestMapping("/hw")
 	public String testHelloWorld(String world) {
 		return "hello, " + world;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/inject")
+	public String testInjection() {
+		return "a: " + testInjectionComponent.getTestProperty();
 	}
 }
