dbbabe5038ec880eb2835c59e04cee96780e063c
hejiafu <hejiafu@secoo.com>
2016-05-04 20:34:26 +0800

    fix fired twice problem

--- src/main/java/study/MyApplicationListener.java
+++ src/main/java/study/MyApplicationListener.java
@@ -1,14 +1,24 @@
 package study;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyApplicationListener implements ApplicationListener<MyEvent> {
+public class MyApplicationListener implements
+		ApplicationListener<ContextRefreshedEvent> {
 
+	/**
+	 * spring will use ContextLoaderListener as parent of applicationContext, so
+	 * event will be fired twice, to avoid this problem, should check whether
+	 * the event is fired by the super root or else.
+	 */
 	@Override
-	public void onApplicationEvent(MyEvent event) {
-		System.out.println("application started: " + event.getEventName());
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (event.getApplicationContext().getParent() == null) {
+			System.out.println("application started: "
+					+ event.getApplicationContext().toString());
+		}
 	}
 
 }
