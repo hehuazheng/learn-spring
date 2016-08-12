42b569333dba3706037b722cb942dce6686710c3
hejiafu <hejiafu@secoo.com>
2016-08-12 15:26:23 +0800

    add

--- src/main/java/study/controllers/TestController.java
+++ src/main/java/study/controllers/TestController.java
@@ -9,11 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.secoo.arch.rabbitmq.RabbitJmsMessageSender;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	@Autowired
 	private AmqpTemplate amqpTemplate;
+	
+	@Autowired
+	private RabbitJmsMessageSender messageSender;
 
 	@ResponseBody
 	@RequestMapping("/hw")
@@ -25,6 +30,22 @@ public class TestController {
 	@RequestMapping("/send")
 	public String testSendMsg(String name, String msg) {
 		amqpTemplate.convertAndSend(name, msg);
+		return "send";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/send1")
+	public String testSendMsg1(String name, String msg) {
+		messageSender.send(name, msg);
+		return "send";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/send2")
+	public String testSendMsg2(String name, String msg) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("data", msg);
+		messageSender.sendJson(name, map);
 		return "send";
 	}
 	
