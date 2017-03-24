240bb46c9fbe465872db364d0824932eb75492fe
hehuazheng <avaforreg@163.com>
2017-03-24 22:23:11 +0800

    logback to log4j add close event on tomcat stop

--- src/main/java/study/controllers/TestController.java
+++ src/main/java/study/controllers/TestController.java
@@ -1,5 +1,7 @@
 package study.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import study.service.SimpleService;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+	private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
 	@Autowired
 	private SimpleService simpleService;
 
@@ -23,5 +27,12 @@ public class TestController {
 	@RequestMapping("/ss")
 	public String testSimpleService() {
 		return simpleService.getName();
+	}
+
+	@ResponseBody
+	@RequestMapping("/log")
+	public String logOut() {
+		LOG.info("test");
+		return "suc";
 	}
 }
